7144b6404173979312f04df39b8ca4a125036904
Mithun Reddy <user@Mithuns-Mac.local>
2025-03-06 22:57:38 +0530

    test branch 2

--- src/main/java/demoproject/Test.java
+++ src/main/java/demoproject/Test.java
@@ -2,9 +2,15 @@ package demoproject;
 
 public class Test {
 	
+	public void test()
+	{
+		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("testing . . . .");
 		System.out.println("Testing class");
+		System.out.println("Testbranch 2 changes ..");
 	}
 
 }
